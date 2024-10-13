7c53edd915d3f8805a2f7d558273ff9c33ec74da
Zahidul Islam Mollik <md.zahidulmollik@gmail.com>
2024-10-14 00:18:23 +0600

    database connected

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -3,5 +3,6 @@ const dotenv=require('dotenv');
 dotenv.config();
 
 module.exports={
-  PORT:process.env.PORT
+  PORT:process.env.PORT,
+  MONGODB_URL:process.env.MONGODB_URL
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 const express=require('express');
-const {envConfig}=require('./config')
+const mongoose=require('mongoose');
+const {envConfig}=require('./config');
 const app=express();
+
 const PORT=envConfig.PORT;
+
+async function connectDB(){
+  try {
+    await mongoose.connect(envConfig.MONGODB_URL);
+    console.log("database connect successfully")
+  } catch (error) {
+    console.log("failed to connect database",error);
+  }
+  
+}
+
+connectDB();
 
 app.listen(PORT,()=>{
   console.log(`Server running on PORT ${PORT}`)
