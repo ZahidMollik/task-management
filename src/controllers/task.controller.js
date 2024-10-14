d8347bcbd62485212e1a3ba4862e0e7beae66eb6
Zahidul Islam Mollik <md.zahidulmollik@gmail.com>
2024-10-14 23:19:00 +0600

    added CRUD operation API

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -7,8 +7,8 @@ async function createTask(req,res){
     const validStatuses = ["pending", "success", "failed"];
     if (status && !validStatuses.includes(status)) {
       return res.status(400).json({ message: 'Invalid status value. Must be one of: pending, success, failed.' });
-    }
-    const newTask= await Task.create({title,description,status,userId:req.user._id});
+    } 
+    const newTask= await Task.create({title,description,status,userId:req.user.id});
     res.status(StatusCodes.CREATED)
       .json({
         status:true,
@@ -26,6 +26,114 @@ async function createTask(req,res){
   
 }
 
+async function getAllTask(req,res){
+  try {
+    const response=await Task.find();
+    res.status(StatusCodes.OK)
+      .json({
+        status:true,
+        message:"Successfully get all tasks",
+        data:response
+     })
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({
+        status:false,
+        message:"something went wrong",
+     })
+  }
+}
+
+async function getTask(req,res){
+  try {
+    const response=await Task.findOne({_id:req.params.id});
+    if(!response){
+      return res.status(StatusCodes.NOT_FOUND)
+      .json({
+        status:false,
+        message:"no task found",
+     })
+    }
+    res.status(StatusCodes.OK)
+      .json({
+        status:true,
+        message:"Successfully get the data",
+        data:response
+     })
+  } catch (error) {
+    
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({
+        status:false,
+        message:"something went wrong",
+     })
+  }
+}
+
+async function updateTask(req,res){
+  const {title,description,status}=req.body;
+  try {
+    const validStatuses = ["pending", "success", "failed"];
+    if (status && !validStatuses.includes(status)) {
+      return res.status(400).json({ message: 'Invalid status value. Must be one of: pending, success, failed.' });
+    } 
+    const response=await Task.updateOne({_id:req.params.id},{title,description,status});
+    if(!response){
+      return res.status(StatusCodes.NOT_FOUND)
+      .json({
+        status:false,
+        message:"no task found for update",
+     })
+    }
+    res.status(StatusCodes.OK)
+      .json({
+        status:true,
+        message:"Successfully update the data",
+        data:response
+     })
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({
+        status:false,
+        message:"something went wrong",
+     })
+  }
+}
+
+async function deleteTask(req,res){
+  try {
+    const response=await Task.deleteOne({_id:req.params.id});
+    if(!response){
+      return res.status(StatusCodes.NOT_FOUND)
+      .json({
+        status:false,
+        message:"no task found for delete",
+     })
+    }
+    res.status(StatusCodes.OK)
+      .json({
+        status:true,
+        message:"Successfully delete the task",
+        data:response
+     })
+  } catch (error) {
+    console.log(error);
+    
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({
+        status:false,
+        message:"something went wrong",
+     })
+  }
+}
+
+
+
+
 module.exports={
-  createTask
+  createTask,
+  getAllTask,
+  getTask,
+  updateTask,
+  deleteTask
 }
--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -4,6 +4,10 @@ const { checkAuth } = require('../middlewares');
 const router=express.Router();
 
 router.post('/task/create',checkAuth,taskController.createTask)
+router.get('/task',checkAuth,taskController.getAllTask)
+router.get('/task/:id',checkAuth,taskController.getTask)
+router.patch('/task/:id',checkAuth,taskController.updateTask)
+router.delete('/task/:id',checkAuth,taskController.deleteTask)
 
 
 module.exports=router;
